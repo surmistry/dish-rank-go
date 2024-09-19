a768d4f4705e7c98b801a5a7c145e58fd4b19b70
Suraj Mistry <mistrs8@gmail.com>
2024-09-19 15:55:41 -0400

    Add dish endpoints

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -17,13 +18,12 @@ type Restaurant struct {
 type Dish struct {
 	Name        string
 	Description string
+	Restaurant  Restaurant
 }
 
 type Review struct {
 	Comment string
 	Dish    Dish
-	// User		User
-	// Asset	Asset
 }
 
 type Ranking struct {
@@ -33,15 +33,50 @@ type Ranking struct {
 }
 
 var rankings []Ranking
+var restaurants []Restaurant
+var dishes []Dish
 
 func greetingsHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Greeting from Go Server 👋")
 }
 
+func getAllRestaurants(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(restaurants)
+}
+
+func addRestaurant(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var restaurant Restaurant
+	json.NewDecoder(r.Body).Decode(&restaurant)
+	restaurants = append(restaurants, restaurant)
+
+	json.NewEncoder(w).Encode(restaurants)
+}
+
+func getAllDishes(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(dishes)
+}
+func addDish(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var dish Dish
+	json.NewDecoder(r.Body).Decode(&dish)
+	dishes = append(dishes, dish)
+
+	json.NewEncoder(w).Encode(dishes)
+}
+
 func main() {
 	r := mux.NewRouter()
-
+	var McDonalds = Restaurant{"McDonalds", "Junk"}
+	restaurants = append(restaurants, McDonalds)
+	dishes = append(dishes, Dish{"McNuggets", "Mixed meat packed into small shapes", McDonalds})
 	r.HandleFunc("/", greetingsHandler).Methods("GET")
+	r.HandleFunc("/restaurants", getAllRestaurants).Methods("GET")
+	r.HandleFunc("/restaurants/add", addRestaurant).Methods("POST")
+	r.HandleFunc("/dishes", getAllDishes).Methods("GET")
+	r.HandleFunc("/dishes/add", addDish).Methods("POST")
 	// r.HandleFunc("/add", addRanking).Methods("POST")
 
 	fmt.Printf("Hello from GoServer 👋")
